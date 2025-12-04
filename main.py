8102dfc3cae1911c7aea39ccd2be3a92ed173bb7
Ce1ikE <enniscelik@gmail.com>
2025-12-04 10:16:19 +0100

    main adjustments

--- main.py
+++ main.py
@@ -26,31 +26,31 @@ def main():
     if not OUTPUT_DIR.exists():
         OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
-    # WBS(
-    #     input_path=INPUT_FILE, 
-    #     output_path_dir=OUTPUT_DIR,
-    # ).build_wbs()
+    WBS(
+        input_path=INPUT_FILE, 
+        output_path_dir=OUTPUT_DIR,
+    ).build_wbs()
 
-    # GanttChart(
-    #     input_path=INPUT_FILE,
-    #     output_path_dir=OUTPUT_DIR,
-    # ).build_milestone_chart_table(
-    #     draw_groups=True,
-    #     base_row_height=0.2,
-    # ).build_deliverables_table(
-    #     draw_groups=True,
-    #     base_row_height=0.2,
-    # ).build_gantt_chart(
-    #     draw_dependencies=True,
-    #     draw_groups=True,
-    #     max_label_size=50,
-    # )
-
-    
-    from scripts.robot_analysis import run
-    run(
-        RESULTS_DIR, DATA_DIR / "cost_data.csv"
+    GanttChart(
+        input_path=INPUT_FILE,
+        output_path_dir=OUTPUT_DIR,
+    ).build_milestone_chart_table(
+        draw_groups=True,
+        base_row_height=0.2,
+    ).build_deliverables_table(
+        draw_groups=True,
+        base_row_height=0.2,
+    ).build_gantt_chart(
+        draw_dependencies=True,
+        draw_groups=True,
+        max_label_size=50,
     )
+
+
+    # from scripts.robot_analysis import run
+    # run(
+    #     RESULTS_DIR, DATA_DIR / "cost_data.csv"
+    # )
 
 
 
